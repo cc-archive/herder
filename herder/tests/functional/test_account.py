78af19ade579b678c8f86eba7930ff60f29fc85d
Nathan R. Yergler <nathan@creativecommons.org>
2008-04-23 15:43:29 -0700

    Test that the login form will be displayed and that we can fake out our logged-in status.

--- herder/tests/functional/test_account.py
+++ herder/tests/functional/test_account.py
@@ -2,6 +2,28 @@ from herder.tests import *
 
 class TestAuthController(TestController):
 
-    def test_index(self):
-        response = self.app.get(url_for(controller='auth'))
-        # Test response...
+    def test_login_form_exists(self):
+        """Ensure the login page contains the form."""
+
+        login_url = url_for(controller='account', action='login')
+        response = self.app.get(login_url)
+
+        print response.body
+        print self.app.post
+        print dir(self.app)
+
+        assert '<form action=' in response.body
+        assert login_url in response.body
+
+    def test_login_override(self):
+        """Ensure we can appear to be logged in (since we need this
+        for other tests)."""
+
+        # claim to be admin
+        self.app.extra_environ['REMOTE_USER'] = 'admin'
+
+        login_url = url_for(controller='account', action='login')
+        response = self.app.get(login_url)
+
+        # make sure the form isn't there
+        assert len(response.forms) == 0
