d3ca863b17ebabba8a501620f31e9fc3e46a2b5f
Asheesh Laroia <asheesh@asheesh.org>
2008-04-24 12:18:12 -0700

    Comments for where I test everything (except the AJAX)

---
+++ herder/tests/integration/test_language.py
@@ -0,0 +1,11 @@
+from herder.test import *
+
+class TestLanguageController(TestController):
+    def test_the_works(self):
+        # Add a language called fake
+        # Note that all the strings are untranslated
+        # (that is, all == untranslated)
+        # Translate one string, and check it goes away from untranslated
+        # Check that it gets stored in a way that we can pull it out?
+
+
