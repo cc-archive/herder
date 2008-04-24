daa9f5470641e1f3649ee2fd8a51b0dfa562afbe
Asheesh Laroia <asheesh@asheesh.org>
2008-04-24 12:08:03 -0700

    test language list, kinda

--- herder/tests/functional/test_domain.py
+++ herder/tests/functional/test_domain.py
@@ -3,5 +3,14 @@ from herder.tests import *
 class TestDomainController(TestController):
 
     def test_index(self):
-        response = self.app.get(url_for(controller='domain'))
-        # Test response...
+        # FIXME: Going to just /domain should return a list of domains
+        #response = self.app.get(url_for(controller='domain'))
+        pass
+
+    def test_list(self):
+        response = self.app.get(url_for(controller='domain', action='cc_org'))
+        assert '.git' not in response.body
+        assert 'fr_CA' in response.body
+        assert 'mk' in response.body
+
+    # FIXME: Figure out other tests...
