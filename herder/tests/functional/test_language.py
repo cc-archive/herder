792ba9ddc796c8b9bd6e20c5314a8b803e52f478
Asheesh Laroia <asheesh@asheesh.org>
2008-05-06 15:10:57 -0700

    Test Language strings controller

--- herder/tests/functional/test_language.py
+++ herder/tests/functional/test_language.py
@@ -8,6 +8,20 @@ class TestLanguageController(TestController):
         response = self.app.get(url_for(controller='language', action='view',
             domain='cc_org', id='en'))
         return True # FIXME: Test something here someday
+    def test_strings_contain(self, desired_key = 'country.us', desired_value='United States'):
+        response = self.app.get(url_for(controller='language', action='strings',
+            domain='cc_org', id='en'))
+        value = jsonlib.read(response.body)
+        strings = value['strings']
+        found_what_we_like = False
+        for stringthing in strings:
+            if stringthing['id'] == desired_key:
+                assert stringthing['value'] == desired_value
+                found_what_we_like = True
+                break
+        assert found_what_we_like
+        return True # FIXME: Test something here someday
+       
     def test_edit_string_as_admin(self):
         # Pretend to be admin
         self.app.extra_environ['REMOTE_USER'] = 'admin'
