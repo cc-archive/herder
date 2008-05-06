35c88f0ef15466bebf6a1fab3eb5a8ffb5921e76
Asheesh Laroia <asheesh@asheesh.org>
2008-05-06 10:30:32 -0700

    Started a skeletal Selenium test

--- herder/tests/integration/test_language.py
+++ herder/tests/integration/test_language.py
@@ -1,12 +1,16 @@
 from herder.tests import *
+import herder.tests
 
 class TestLanguageController(TestController):
     def test_the_works(self):
         # First, pretend to be the admin user.
+        herder.tests.start_selenium()
+        herder.tests.SELENIUM_BROWSER.open('http://localhost:5001/')
         
         # Add a language called fake
         # Note that all the strings are untranslated
         # (that is, all == untranslated)
         # Translate one string, and check it goes away from untranslated
         # Check that it gets stored in a way that we can pull it out?
+        herder.tests.stop_selenium()
         pass
