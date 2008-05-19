43decb5ab7f02b79f3fa94a417e99cf45918adfa
Asheesh Laroia <asheesh@asheesh.org>
2008-05-19 12:20:16 -0700

    Add a test for suggestions as a non admin

--- herder/tests/functional/test_language.py
+++ herder/tests/functional/test_language.py
@@ -66,4 +66,26 @@ class TestLanguageController(TestController):
 
         # Stop pretending to be an admin.
         del self.app.extra_environ['REMOTE_USER']
-        
+
+'''    def test_make_suggestion_as_non_admin(self):
+        # Pretend to be admin
+        self.app.extra_environ['REMOTE_USER'] = 'joeuser'
+        # First, change it so old -> new
+        i18n_key = 'country.us'
+        old = 'United States'
+        new = 'Untied States'
+        # Just check that things are the way we thought
+        self.test_strings_contain(desired_key=i18n_key, desired_value=old)
+
+        # Now try to do the edit
+        url_indeed = url_for(controller='language', action='edit_string',
+            domain='cc_org', id='en_US')
+        response = self.app.post(url_indeed, 
+            params={'data': 
+            jsonlib.write({'id': i18n_key,
+                'new_value': new, 'old_value': old})})
+        # And, uh, FIXME - right now we have no way of checking if it stuck
+
+        # At least, the real string shouldn't have changed - repeat this check
+        self.test_strings_contain(desired_key=i18n_key, desired_value=old)       
+'''
