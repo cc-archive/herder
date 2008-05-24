9411d00f742fc2b695f22eb75765342d436bdff5
Asheesh Laroia <asheesh@asheesh.org>
2008-05-23 17:05:57 -0700

    Give the user table a reasonable name now that it works

--- herder/model/user.py
+++ herder/model/user.py
@@ -4,7 +4,7 @@ import random
 
 from herder.model import meta
 
-t_user = sa.Table("wtf_User", meta.metadata,
+t_user = sa.Table("user", meta.metadata,
                       sa.Column('user_id', sa.types.Integer, primary_key = True, autoincrement = True),
                       sa.Column('user_name', sa.types.Unicode(255), nullable = False),
                       sa.Column('salt', sa.types.String(255), nullable = False),
