9a349e20b4ee35049fd46ca21547ff969802018e
paulproteus <paulproteus@a7.creativecommons.org>
2008-07-29 20:09:01 +0000

    Add BeautifulSoup dependency

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     install_requires=['setuptools',
                       'pudge',
                       'buildutils',
+                      'BeautifulSoup',
                       'Pygments',
                       'Pylons>=0.9.6.1', 
                       'Babel',
