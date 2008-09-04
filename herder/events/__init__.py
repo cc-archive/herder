dc0f796beee284cb70ac3b2786b772d2f9e8e492
Nathan R. Yergler <nathan@creativecommons.org>
2008-09-04 12:03:29 -0700

    Convenience handler method.

--- herder/events/__init__.py
+++ herder/events/__init__.py
@@ -1,4 +1,10 @@
-import handlers
+import zope.component
 
+import handlers
 from events import MessageUpdateEvent
 
+def handle(event):
+    """Convenience method to dispatch events into the "real" event 
+    handling mechanism."""
+
+    return zope.component.handle(event)
