aac968c2b524e61164207b82a01d2e70ad83a455
Asheesh Laroia <asheesh@asheesh.org>
2008-09-03 17:11:16 -0700

    Making websetup interactive (but want to make it noninteractive for test mode)

--- herder/websetup.py
+++ herder/websetup.py
@@ -6,10 +6,12 @@ from pylons import config
 
 from herder.config.environment import load_environment
 import herder
+import herder.controllers.account
 
 log = logging.getLogger(__name__)
 
-def setup_config(command, filename, section, vars):
+def setup_config(command, filename, section, vars,
+                 bureaucrat_username = None, bureaucrat_password = None):
     """Place any commands to setup herder here"""
     conf = appconfig('config:' + filename)
     load_environment(conf.global_conf, conf.local_conf)
@@ -26,3 +28,34 @@ def setup_config(command, filename, section, vars):
     herder.model.meta.Session.save(bureau_role)
     herder.model.meta.Session.save(translator_role)
     herder.model.meta.Session.commit()
+
+    if bureaucrat_username is None:
+        bureaucrat_username = raw_input(
+            'Hey there.  What username do you want for your bureaucrat user? > ')
+        bureaucrat_realname = raw_input(
+            'Great.  What is the actual name of this person? > ')
+
+    if bureaucrat_password is None:
+        bureaucrat_password = herder.model.user.random_alphanum()
+    
+    # make the new bureaucrat user
+    new_user = herder.model.user.User()
+    new_user.user_name = unicode(bureaucrat_username)
+    new_user.salt = herder.model.user.random_alphanum()
+    new_user.hashed_salted_pw = herder.model.user.hash_with_salt(
+        salt=new_user.salt,
+        raw_password=bureaucrat_password)
+    new_user.human_name = bureaucrat_realname
+    herder.model.meta.Session.save(new_user)
+
+    # Flush the session...
+    herder.model.meta.Session.commit()
+
+    # and bless him with magical powers
+    herder.controllers.account.bless_user(new_user)
+
+    # And say what we've done.
+    print 'Congratulations.  Your password is:', bureaucrat_password
+
+
+    
