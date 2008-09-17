9be187725c1985510b65f2ad4191e0771cc4f09e
Asheesh Laroia <asheesh@asheesh.org>
2008-09-17 15:45:12 -0700

    Use domain_id to refer to the name of the domain in the event object

--- herder/events/events.py
+++ herder/events/events.py
@@ -12,12 +12,12 @@ class MessageUpdateEvent(HerderEvent):
                    message.language.lang,
                    message.id)
 
-    def __init__(self, domain_name, lang_id, message_id):
+    def __init__(self, domain_id, lang_id, message_id):
 
-        self.domain_name = domain_name
+        self.domain_id = domain_id
         self.lang_id = lang_id
         self.message_id = message_id
 
     def __str__(self):
         return "%s: %s: %s updated" % (
-            self.domain_name, self.lang_id, self.message_id)
+            self.domain_id, self.lang_id, self.message_id)
