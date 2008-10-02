a73c1ce824b377ff687c0dab11388c261da68711
Asheesh Laroia <asheesh@asheesh.org>
2008-10-02 15:59:25 -0700

    Might as well salt both sides of the hash

--- herder/model/user.py
+++ herder/model/user.py
@@ -27,7 +27,7 @@ def random_alphanum(length = 12):
 
 import sha
 def hash_with_salt(salt, raw_password):
-    return sha.sha(salt + raw_password).hexdigest()
+    return sha.sha(salt + raw_password + salt).hexdigest()
 
 import md5
 def hash_oldskool(raw_password):
