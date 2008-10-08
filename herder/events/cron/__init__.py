43debe1d5bb6767688c62301590d253107343a19
Asheesh Laroia <asheesh@asheesh.org>
2008-10-08 14:37:22 -0700

    We don't need the handle() in events.cron

--- herder/events/cron/__init__.py
+++ herder/events/cron/__init__.py
@@ -1,12 +1,2 @@
-import events
-import zope.component
-
 import handlers
 from events import *
-
-def handle(event):
-    """Convenience method to dispatch events into the "real" event 
-    handling mechanism."""
-
-    return zope.component.handle(event)
- 
