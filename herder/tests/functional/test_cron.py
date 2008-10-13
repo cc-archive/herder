f5c7ebdf697c3f94d1868635d750247bd3c00def
Asheesh Laroia <asheesh@asheesh.org>
2008-10-13 15:21:52 -0700

    Add test for monthly email sending out

--- herder/tests/functional/test_cron.py
+++ herder/tests/functional/test_cron.py
@@ -2,13 +2,11 @@
 import herder.tests
 from minimock import Mock
 from herder.tests import *
-import herder.tests.functional.test_account
 from herder.tests.functional.test_language import TestLanguageController
-from herder.tests.functional.test_account import TestProfilePrefLangSpecificMail
+from herder.tests.functional.test_account import do_register
 import herder.events.cron.cli
 import smtplib
 from herder.tests.functional.test_language import TestLanguageController
-import herder.controllers.language
 
 class TestCron(TestController):
     def test_monthly_email_sans_suggestions(self):
@@ -26,6 +24,20 @@ class TestCron(TestController):
         >>>
         # Log in as some new user
         >>> u, p, e, n = [herder.model.user.random_alphanum() for k in range(4)]
-        >>> lc = herder.controllers.language.LanguageController()
-        >>> 
+        >>> tlc = TestLanguageController()
+        >>> do_register(tlc.app, user_name=u, password=p, human_name=n, email=e + '@example') #doctest: +ELLIPSIS
+        '...'
+        >>> tlc.login_as(u, p) #doctest: +ELLIPSIS
+        <Response 200 ...>
+        >>> tlc.test_make_suggestion_as_non_bureau(already_logged_in_as=u, do_suggest=True, do_delete=False)
+        >>> herder.events.cron.cli.cron(['-c', 'test.ini', '-m']) #doctest: +ELLIPSIS
+        Emailing out monthly status reminders...
+        Called smtplib.SMTP('localhost')
+        Called smtp_connection.sendmail(
+            'herder-bounces@localhost',
+            ['bureau@example.com'],
+            '...pending suggestions...country.us')
+        Called smtp_connection.quit()
+        ...success.
+        >>> tlc.test_make_suggestion_as_non_bureau(already_logged_in_as=u, do_suggest=False, do_delete=True)
         '''
